fea505af18c092cdd62b679fcc5344a34479604a
Shawn Lentz <shawn.lentz@gmail.com>
2021-06-20 22:33:03 -1000

    Changed from flexbox to grid

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -1,1 +1,29 @@
 console.log("Srripts loading...")
+
+// Operation functions
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(operation, a, b) {
+    //pass
+}
+
+function outPutToDisplay(toDisplay) {
+    document.querySelector('#display').innerText = toDisplay;
+}
+
+outPutToDisplay()
