154a71c5d732da77e0edd1b68024976b131479e2
Shawn Lentz <shawn.lentz@gmail.com>
2021-07-02 12:26:25 -1000

    Basic operations work, need to add multiple operations

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -1,142 +1,113 @@
 console.log("Scripts loading...")
-let num1;
-let num2;
-let solution;
-let operator;
-// console.log("Operator", operator)
-// Operation functions
+// Get all the buttons
 
-function add(a, b) {
-    return a + b
-}
-
-function subtract(a, b) {
-    return a - b
-}
-
-function multiply(a, b) {
-    return a * b
-}
-
-function divide(a, b) {
-    return a / b
-}
-
-function operate(operation, a, b) {
-
-    return operation(a, b);
-
-}
-
-
-function outPutToDisplay(toDisplay) {
-
-    if (toDisplay.match(/^[0-9]+$/i)) {
-        if (document.querySelector('#display').innerText === '0') {
-            document.querySelector('#display').innerText = '';
-        }
-        document.querySelector('#display').innerText += toDisplay
+// Lets try a class
+class Calculator {
+    constructor(currentOperand, previousOperand) {
+        this.currentOperand = currentOperand
+        this.previousOperand = previousOperand
+        this.operator = undefined
+        this.clear()
     }
-}
 
-function buttonPress() {
-    let display = document.querySelector('#display')
-    display.innerText = '';
-    let button = document.addEventListener('click', (e) => {
-        console.log("Pressing button")
-        if (e.target.innerText.match(/^[0-9]+$/i)) {
-            outPutToDisplay(e.target.innerText)
-        } 
-        return parseInt(e.target.innerText)
-    })
-}
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operator = undefined
+        calcDisplay.innerText = '0'
+        operDisplay.innerText = '';
 
-function addOperators() {
-    // Add operators to operator buttons
+    }
 
-    document.getElementsByName('divide')[0].addEventListener('click', () => {
-        console.log("Dividing!")
-    })
-    document.getElementsByName('add')[0].addEventListener('click', () => {
-        console.log("Adding!")
-        // When an operator button is clicked, begin the 'operation', capture
-        // the first number and the operator, then get the second number.
-        // Then, when you hit 'equals', it should run the operate()/function
-        // Addition is just an easy example to work with.
-        num1 = parseInt(document.querySelector('#display').innerText)
+    operate() {
+        console.log("IN OPERATOR", this.operator, this.previousOperand, this.currentOperand)
+        if (this.operator === '+') {
+            return this.currentOperand + this.previousOperand
+        }
+        if (this.operator === '-') {
+            return this.previousOperand - this.currentOperand
+        }
+        if (this.operator === '÷') {
+            return this.previousOperand / this.currentOperand
+        }
+        if (this.operator === 'x') {
+            return this.currentOperand * this.previousOperand
+        }
+    }
 
-        // Choose any digit should then clear the display and start a new number
-        let buttons = document.querySelectorAll('.digit')
+    updateDisplay(number) {
+        if (calcDisplay.innerText === '0') {
+            calcDisplay.innerText = number
+        } else {
+            calcDisplay.innerText += number
+        }
 
-        buttons.forEach(button => {
-            button.addEventListener('click', (e) => {
-                let display = document.querySelector('#display')
-
-                // display.innerText = ''
-                num2 = parseInt(e.target.innerText)
-                console.log("NUMBER2", num2)
-                display.textContent = '';
-                // display.textContent = num2
-                console.log("display", display)
-                solution = operate(add, num1, num2)
-                // num1 = solution
+        if (operDisplay.innerText !== '') {
+            operDisplay.innerText += number
+        }
+    }
 
 
-            })
-        })
-
-    })
-    document.getElementsByName('subtract')[0].addEventListener('click', () => {
-        console.log("Subtracting!")
-    })
-    document.getElementsByName('multiply')[0].addEventListener('click', () => {
-        console.log("Multiplying!")
-    })
-
-    // Clear functions
-    document.getElementsByName('allclear')[0].addEventListener('click', () => {
-        // Completely clear all numbers
-        num1 = 0;
-        num2 = 0;
-        solution = 0;
-        document.querySelector('#display').innerText = 0;
-    })
-
-    document.getElementsByName('clear')[0].addEventListener('click', () => {
-        // Clear num1 and num2 but keep solution 
- 
-        num1 = 0;
-        num2 = 0;
-        document.querySelector('#display').textContent = 0;
-    })
-    
-}
-
-function getSecondNum() {
-    let display = document.querySelector('#display')
-    display.innerText = '';
-    buttonPress()
 
 }
 
-function calculate(operator, num) {
-    //Main function
-    console.log("Launching calculator")
+let digits = document.querySelectorAll('.digit')
+let operators = document.querySelectorAll('.operator')
+let allclear = document.getElementsByName('allclear')[0]
+let clear = document.getElementsByName('clear')[0]
+let equals = document.getElementById('equals')
+let currentOperand = '';
+let previousOperand = '';
+let calcDisplay = document.querySelector('#display')
+let operDisplay = document.querySelector('#smaller-display')
+// console.log(digits, operators, allclear, clear, equals)
 
-}
+const calculator = new Calculator()
 
-function equals() {
-    let equal = document.querySelector('#equals')
-    let display = document.querySelector('#display')
-    equal.addEventListener('click', () => {
-        display.innerText = '';
-        display.innerText = solution
-    })
-}
+// Add digit updates to display
+digits.forEach(digit => {digit.addEventListener('click', () => {
+    calculator.updateDisplay(digit.innerText)
+})})
 
-equals()
-buttonPress()
-addOperators()
-outPutToDisplay('0')
-calculate()
+// Add clear to clear button
+allclear.addEventListener('click', () => {
+    calculator.clear()
+})
+
+// Add operator listeners
+operators.forEach(operator => {operator.addEventListener('click', () => {
+    calculator.operator = operator.innerText
+
+    if (calcDisplay.innerText === '0') {
+        //pass
+        // console.log("Passing")
+    } else if (calcDisplay !== '' && calculator.previousOperand === '') {
+        calculator.previousOperand = parseInt(calcDisplay.innerText)
+        operDisplay.innerText = calculator.previousOperand + calculator.operator
+        calcDisplay.innerText = '0'
+    } else if(calcDisplay !== '' && calculator.previousOperand !== '') {
+        calculator.currentOperand = parseInt(calcDisplay.innerText)
+        solution = calculator.operate()
+        calcDisplay.innerText = solution
+    }
+
+})})
+
+// Add equals operation
+equals.addEventListener('click', () => {
+    if (calculator.currentOperand === '' && calculator.previousOperand === '') {
+        console.log("Not adding")
+    } else {
+        calculator.currentOperand = parseInt(calcDisplay.innerText)
+        solution = calculator.operate()
+        calcDisplay.innerText = solution;
+    }
+    calculator.currentOperand = '';
+    calculator.previousOperand = '';
+})
+
+
+
+
+
 
