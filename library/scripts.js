e33bf63989fb74b7d03ad6c936060bb2c717c764
Shawn Lentz <shawn.lentz@gmail.com>
2021-07-08 10:01:20 -1000

    Started work on modal and new boook form

--- library/scripts.js
+++ library/scripts.js
@@ -1,1 +1,16 @@
 console.log("Scripts loading....")
+
+// NEW BOOK FORM AND MODAL
+
+let newBook = document.getElementsByClassName('new-book')[0]
+newBook.addEventListener('click', () => {
+    let modal = document.querySelector('.modal')
+    let closeButton = document.querySelector('.close-btn')
+    modal.style.display = 'block'
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+    })
+})
+
+
+console.log(newBook)
