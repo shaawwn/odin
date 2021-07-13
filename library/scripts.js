b8996a5d161600835d9cac17bb61fa219bb777f6
Shawn Lentz <shawn.lentz@gmail.com>
2021-07-13 08:34:23 -1000

    Removed old code, still need to style

--- library/scripts.js
+++ library/scripts.js
@@ -4,44 +4,6 @@ let library = [];
 let libraryContent = document.getElementById('library-content')
 let libraryStorage = window.localStorage
 
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    // let removeButton = document.querySelectorAll('.remove-button')
-    // removeButton.forEach(button => button.addEventListener('click', () => {
-    //     let keyName = button.parentElement.children[0].innerText
-
-    //     //Remove object from local storage
-    //     localStorage.removeItem(keyName)
-
-    //     // Remove the HTML
-    //     removeItem(button.parentElement)
-    // }))
-
-    // let readButton = document.querySelectorAll('.read-book')
-    // readButton.forEach(button => button.addEventListener('click', () => {
-    //     console.log("Read book!")
-    //     // Need to actually toggle the object data not just HTML
-    //     let bookObjectKey = button.parentElement.children[0].innerText;
-    //     let bookObject = JSON.parse(localStorage.getItem(bookObjectKey))
-    //     // console.log(bookObject)
-
-    //     if (button.innerText === 'Read') {
-    //         bookObject.bookRead = "Not Read"
-    //         button.innerText = 'Not Read'
-    //         console.log("Book is read", bookObject)
-
-    //         localStorage.setItem(bookObjectKey, JSON.stringify(bookObject))
-    //     } else if (button.innerText === 'Not Read') {
-    //         bookObject.bookRead = 'Read'
-    //         button.innerText = 'Read'
-    //         console.log("Book is not read", bookObject)
-
-    //         localStorage.setItem(bookObjectKey, JSON.stringify(bookObject))
-    //     }
-    // }))
-})
-
 // NEW BOOK AND MODAL
 let newBook = document.getElementsByClassName('new-book')[0]
 let submitButton = document.getElementById('submit-button')
@@ -66,45 +28,12 @@ function Book(title, author, pages, read) {
     this.bookRead = read
 }
 
-function displayLibrary() {
-    // Loop through the library and add the books
-    libraryContent.innerHTML = '';
-    for (let i = 0; i < library.length; i++) {
-        let bookDiv = document.createElement('div')
-        bookDiv.classList.add('book-card')
-
-        let bookTitle = document.createElement('h2');
-        let bookAuthor = document.createElement('h4')
-        let bookPages = document.createElement('h4')
-        let bookRead = document.createElement('h4')
-
-        bookTitle.innerText = library[i].bookTitle;
-        bookAuthor.innerText = library[i].bookAuthor;
-        bookPages.innerText = library[i].bookPages;
-        bookRead.innerText = library[i].bookRead;
-
-        bookDiv.append(bookTitle);
-        bookDiv.append(bookAuthor);
-        bookDiv.append(bookPages);
-        bookDiv.append(bookRead);
-
-        libraryContent.appendChild(bookDiv)
-
-    }
-}
-
 function displayLibraryStorage() {
     // Loop through the library and add the books
     libraryContent.innerHTML = '';
-    // console.log("IN LS FUNC", libraryStorage)
     for (let i = 0; i < libraryStorage.length; i++) {
-        // console.log(JSON.parse(libraryStorage.getItem(libraryStorage.key(i))))
         let book = JSON.parse(libraryStorage.getItem(libraryStorage.key(i)))
         
-        // console.log(book.bookTitle)
-        // console.log("Author: ", book.bookAuthor)
-        // console.log("Pages: ", book.bookPages)
-        // console.log("Read: ", book.bookRead)
         let bookDiv = document.createElement('div')
         bookDiv.classList.add('book-card')
 
@@ -113,10 +42,7 @@ function displayLibraryStorage() {
         let bookPages = document.createElement('h4')
         let bookRead = document.createElement('button')
         let removeButton = document.createElement('button')
-        // let removeButton2 = document.createElement('button')
 
-        // removeButton.classList.add('remove-book')
-        // removeButton.classList.add('btn')
 
         removeButton.classList.add('remove-button')
         removeButton.classList.add('btn')
@@ -127,7 +53,7 @@ function displayLibraryStorage() {
 
         bookTitle.innerText = book.bookTitle;
         bookAuthor.innerText = book.bookAuthor;
-        bookPages.innerText = book.bookPages;
+        bookPages.innerText = book.bookPages + ' pages';
         bookRead.innerText = book.bookRead;
 
         // Add event listeners
@@ -178,7 +104,6 @@ function addToLibrary() {
 
     library.push(newerBook)
     displayLibraryStorage()
-    // displayLibrary()
 
     return false
 }
@@ -214,19 +139,8 @@ function clearStorage() {
 function removeItem(element) {
     console.log("Removing item", element)
     element.remove()
-    // localStorage.removeItem(e)
 
 }
-
-// Storage
-
-// Setting a book with the relevant information example
-// setItem() takes two arguments, second use
-// libraryStorage.setItem('book', JSON.stringify({'title': 'The Hobbit', 'author': 'JRR Tolkein'}))
-// console.log(JSON.parse(libraryStorage.getItem('book')).title)
-
-// Clear storage with clear()
-// libraryStorage.clear()
 
 clearStorage()
 displayLibraryStorage()
